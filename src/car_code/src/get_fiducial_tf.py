0b6f04196025c91d8c0c4accedffe316c4a312de
YuLin Chen <r08522622@ntu.edu.tw>
2020-03-06 17:07:44 +0800

    fiducial - tf code ok!!

--- src/car_code/src/get_fiducial_tf.py
+++ src/car_code/src/get_fiducial_tf.py
@@ -4,11 +4,14 @@ from std_msgs.msg import Float64
 from fiducial_msgs.msg import FiducialTransformArray, FiducialTransform
 import tf
 
-class sub():
+class aruco_detect():
     def __init__(self):
         rospy.init_node('Get_Fiducial_Transforms', anonymous=True)
         rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.get_fidu_tf)
-        # self.list = FiducialTransform()
+        # tf part
+        self.br = tf.TransformBroadcaster()
+        self.rate = rospy.Rate(10)
+
 
     def get_fidu_tf(self, msg):
         self.currentSeq = msg.image_seq
@@ -18,18 +21,24 @@ class sub():
         self.aruco_id = self.aruco.fiducial_id
         self.aruco_tf_trans = self.aruco_tf.translation
         self.aruco_tf_rot   = self.aruco_tf.rotation
-
-        # tf data:
-        # x = self.aruco_tf_trans.x
-        # y = self.aruco_tf_trans.y
-        # z = self.aruco_tf_trans.z
+        # print(type(self.aruco_tf_trans.x))
         
-
+        # tf data:
+        # x = self.aruco_tf_trans.x  # red axis in RViz
+        # y = self.aruco_tf_trans.y  # green axis in RViz
+        # z = self.aruco_tf_trans.z  # blue axis in RViz
+        
+        self.br.sendTransform(  (self.aruco_tf_trans.x, self.aruco_tf_trans.y, self.aruco_tf_trans.z), 
+                                (self.aruco_tf_rot.x, self.aruco_tf_rot.y, self.aruco_tf_rot.z, self.aruco_tf_rot.w), 
+                                rospy.Time.now(), 
+                                "aruco_%d" %self.aruco_id, 
+                                "camera")
+        self.rate.sleep()
 
 
 if __name__ == '__main__':
     try:
-        a = sub()
+        a = aruco_detect()
         while not rospy.is_shutdown():
             pass
 
